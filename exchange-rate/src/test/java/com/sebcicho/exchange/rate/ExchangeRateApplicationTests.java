e50be5126904091a2d36b367b2d0f3e57563b6f9
sebcicho <sebastian.cichocki1g@gmail.com>
2022-10-18 21:58:08 +0200

    initial commit

---
+++ exchange-rate/src/test/java/com/sebcicho/exchange/rate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebcicho.exchange.rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
